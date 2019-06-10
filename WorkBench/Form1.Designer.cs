29d2a9c02b614f43499ec4960855bdc5e607ae35
Bo Wang <wangbo03@inspur.com>
2019-06-10 19:48:16 +0800

    Init

---
+++ WorkBench/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WorkBench
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.globalEventProvider1 = new Gma.UserActivityMonitor.GlobalEventProvider();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(664, 342);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(694, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(104, 411);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "GSCloud WorkBench";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Gma.UserActivityMonitor.GlobalEventProvider globalEventProvider1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ WorkBench/Form1.cs
@@ -0,0 +1,89 @@
+﻿using Gma.UserActivityMonitor;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WorkBench.Properties;
+
+namespace WorkBench
+{
+
+    public partial class Form1 : Form
+    {
+        private Keys prevKey=Keys.None;
+        private DateTime prevTime;
+        public Form1()
+        {
+            InitializeComponent();
+            SetNotifier();
+            HookManager.KeyDown += new KeyEventHandler(hook_KeyDown);
+        }
+        void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (prevKey == Keys.LControlKey)
+            {
+                if (e.KeyCode == Keys.LControlKey)
+                {
+                    var interval = DateTime.Now - prevTime;
+                    if (interval.Milliseconds < 300 && interval.Milliseconds > 50)
+                    {
+                        prevKey = Keys.None;
+                        ChageState();
+                        return;
+                    }
+                }
+            }
+            prevKey = e.KeyCode;
+            prevTime = DateTime.Now;
+        }
+        private void ChageState()
+        {
+            if (this.Visible==false)
+            {
+                ShowNormal();
+            }
+            else
+            {
+                GotoTray();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState== FormWindowState.Minimized)
+            {
+                GotoTray();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GotoTray();
+            e.Cancel = true;
+        }
+        private void GotoTray() {
+            this.Hide();
+            this.notifyIcon1.Visible = true;
+           // this.notifyIcon1.ShowBalloonTip(500, "提示", "呵呵", ToolTipIcon.Info);
+        }
+        private void ShowNormal()
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            if (this.Visible == false)
+            {
+                this.Visible = true;
+            }
+        }
+        private void SetNotifier() {
+            this.notifyIcon1.Icon = Resources.tray;
+            this.notifyIcon1.Text = "WorkBench";
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
